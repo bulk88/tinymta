e1bba42198354804b6f7320cc90294f802d64020
bulk88 <bulk88@hotmail.com>
2017-10-24 10:55:59 -0400

    directly load station for counties with 1 station for MNRR
    -New Cannan in Bronx is only example
    -Similar to LIRR Manhattan and Subway L in Queens tap/click save

--- mnr/gr3.pl
+++ mnr/gr3.pl
@@ -33,7 +33,7 @@ binmode(HTMLFILE);
 print #mobileoptimized for IE Mobile 6 text wrapping/zoom behavior, otherwise route names dont wrap and scrolling required
 #a tiny bit of CSS needed so the buttons dont waste vertical space with 16px margin
 $mob ?
-'<html><head><meta name="mobileoptimized" content="0"></head><body><style>form{margin-bottom:0px;margin-top:0px;}</style><a name="#">
+'<html><head><meta name="mobileoptimized" content="0"></head><body><style>form{margin-bottom:0px;margin-top:0px}input{padding-top:0px;padding-bottom:0px}div form{display:inline}</style><a name="#">
 ':'<html><head><meta name="mobileoptimized" content="0"></head><body><a name="#">
 ';
 foreach my $routename (sort keys %$VAR1) {
@@ -53,26 +53,39 @@ foreach my $routename (sort keys %$VAR1) {
     }
     my $line = "<a name=\"$rtnum\">$routename:";
     foreach my $borough (sort keys %boroughs) {
-        $line .= ' <a href="#'.$rtnum.$borotbl{$borough}.'">'.$borough.'</a>';
-        push(@linestopshtml, "$routename: $borough <a name=\"".$rtnum.$borotbl{$borough}."\" href=\"#\">Home</a>");
+        if(@{$boroughs{$borough}} > 1){
+            $line .= ' <a href="#'.$rtnum.$borotbl{$borough}.'">'.$borough.'</a>';
+            push(@linestopshtml, "$routename: $borough <a name=\"".$rtnum.$borotbl{$borough}."\" href=\"#\">Home</a>");
+        } else { #if 1 station per boro, just jump straight to station, saves a tap
+            my $name = ${$boroughs{$borough}}[0]->{name};
+            my $stopid = ${$boroughs{$borough}}[0]->{stop};
+            $line .= ' '.stopid_to_tag($name, $stopid, $borough);
+            push(@linestopshtml, "$routename: $borough <a href=\"#\">Home</a>");
+        }
         foreach my $stopidx (0..@{$boroughs{$borough}}-1) {
             my $name = ${$boroughs{$borough}}[$stopidx]->{name};
             my $stopid = ${$boroughs{$borough}}[$stopidx]->{stop};
-            push(@linestopshtml, ($mob?
-'<form action="http://m.mta.info/mt/as0.mta.info/mnr/mstations/station_status_display.cfm" method="post">
-<input value="'.$name.'" type="submit">
-<input value="'.$stopid.','.$name.'" name="P_AVIS_ID" type="hidden">
-</form>'
-                                  :('<a href="http://as0.mta.info/mnr/mstations/station_status_display.cfm?P_AVIS_ID='
-                 .$stopid.','.$name.'">'.$name.'</a>')));
+            push(@linestopshtml, stopid_to_tag($name, $stopid, $name));
         }
     }
     push(@linesboroughhtml,$line);
 }
+
+sub stopid_to_tag { #$href_attr = stopid_to_tag($name, $stopid, $dispname)
+my ($name, $stopid, $dispname) = @_;
+return ($mob?
+'<form action="http://m.mta.info/mt/as0.mta.info/mnr/mstations/station_status_display.cfm" method="post">
+<input value="'.$dispname.'" type="submit">
+<input value="'.$stopid.','.$name.'" name="P_AVIS_ID" type="hidden">
+</form>'
+                                  :('<a href="http://as0.mta.info/mnr/mstations/station_status_display.cfm?P_AVIS_ID='
+                 .$stopid.','.$name.'">'.$dispname.'</a>'));
+}
+
 print join(" \n", @lineshtml);
-print "\n<br>\n".($mob?'<a href="stations.htm">MTA No Mobile</a>':'<a href="stationsmob.htm">MTA Mobile</a>')."<br>\n";
+print "\n<br>\n".($mob?'<a href="stations.htm">MTA No Mobile</a>'."<br>\n<div>\n":'<a href="stationsmob.htm">MTA Mobile</a>'."<br>\n");
 print join("<br>\n", @linesboroughhtml);
-print "\n<br><br>\n";
+print $mob?"\n<br>\n</div>\n<br>\n":"\n<br><br>\n";
 print join($mob?"\n":"<br>\n", @linestopshtml);
 print "\n".'</body></html>';
 $Data::Dumper::Sortkeys = 1;
