62d601384fcaf7103c3acdf85ccd5051df51025d
bulk88 <bulk88@hotmail.com>
2017-11-12 16:28:30 -0500

    directly load station for counties with 1 station for MNRR dumbphone
    -New Cannan in Bronx is only example
    -Similar to LIRR Manhattan and Subway L in Queens tap/click save
    -MNR grmp.pl is most "advanced" one now since it generates stop pages in
     1 pass instead of 2
    -add CSS to per-route page for buttons

--- mnr/grmp.pl
+++ mnr/grmp.pl
@@ -45,50 +45,65 @@ foreach my $routename (@routes) {
     }
     @boroughs = sort keys %boroughs;
     my $rtfile = "$routename:";
+    my $rtfileheader = '';
     my $accesskeyidx = 1; #only 5 boroughs so no overflow check
     foreach my $borough (@boroughs) {
-        $rtfile .= ' <a accesskey='.$accesskeyidx++.' href="'.$rtnum.$borotbl{$borough}.'.htm">'.$borough.'</a>';
-        my @borohtml;
-        foreach my $stopidx (0..@{$boroughs{$borough}}-1) {
-            my $name = ${$boroughs{$borough}}[$stopidx]->{name};
-            my $stopid = ${$boroughs{$borough}}[$stopidx]->{stop};
-            push(@borohtml, ($mob?
-'<form action="http://m.mta.info/mt/as0.mta.info/mnr/mstations/station_status_display.cfm" method="post">
-<input value="'.$name.'" type="submit">
-<input value="'.$stopid.','.$name.'" name="P_AVIS_ID" type="hidden">
-</form>'
-                                  :('<a href="http://as0.mta.info/mnr/mstations/station_status_display.cfm?P_AVIS_ID='
-                 .$stopid.','.$name.'">'.$name.'</a>')));
-        }
-        my $boropages = ceil(scalar(@borohtml) / 9);
-        my $pageidx;
-        while (my @dumbborohtml = splice @borohtml, 0, 9) { #break down stop list into chunks of 9
-            my $borofile = "$routename: $borough".($boropages > 1 ? ' '.($pageidx+1).' of '.$boropages:'')."<br>\n";
-            my $accesskeyidx = 1;
-            foreach my $stopline (@dumbborohtml) {
-                my $ak = 'accesskey='.$accesskeyidx++;
-                if($mob) {
-                    $stopline =~ s/type="submit">/$ak type="submit">/;
-                } else {
-                    $stopline =~ s/<a /<a $ak /;
+        if(@{$boroughs{$borough}} > 1 ) {
+            $rtfile .= ' <a accesskey='.$accesskeyidx++.' href="'.$rtnum.$borotbl{$borough}.'.htm">'.$borough.'</a>';
+            my $boropages = ceil(scalar(@{$boroughs{$borough}}) / 9);
+            my $pageidx;
+            while (my @stopschunk = splice @{$boroughs{$borough}}, 0, 9) { #break down stop list into chunks of 9
+                my $borofile = "$routename: $borough".($boropages > 1 ? ' '.($pageidx+1).' of '.$boropages:'')."<br>\n";
+                my $accesskeyidx = 1;
+                foreach my $stop (@stopschunk) {
+                    $borofile .= stopid_to_tag($stop->{name},
+                        $stop->{stop},
+                        $stop->{name},
+                        '', #no anchor
+                        $accesskeyidx++);
                 }
-                $borofile .= $stopline;
+                if(@{$boroughs{$borough}}) {
+                    $borofile .= '<a accesskey=0 href='.$rtnum.$borotbl{$borough}.($pageidx+1).'.htm>More</a>'."\n";
+                }
+                write_html('../docs/mn/'.($mob?'m/':'').$rtnum.$borotbl{$borough}.$pageidx.'.htm', '<style>form{margin-bottom:0px;margin-top:0px;}</style>'.$borofile);
+                $pageidx++;
             }
-            if(@borohtml) {
-                $borofile .= '<a accesskey=0 href='.$rtnum.$borotbl{$borough}.($pageidx+1).'.htm>More</a>'."\n";
+        } else {    #if 1 station per boro, just jump straight to station
+                    #saves a tap, only L/Queens/Halsey has this property
+            $rtfile .= ' '.stopid_to_tag(${$boroughs{$borough}}[0]->{name},
+                          ${$boroughs{$borough}}[0]->{stop},
+                          $borough,
+                          '', #no anchor
+                          $accesskeyidx++);
+            if($mob && !$rtfileheader) { #otherwise button bad formatting
+                $rtfileheader = '<style>form{margin-bottom:0px;margin-top:0px;}</style>';
             }
-            write_html('../docs/mn/'.($mob?'m/':'').$rtnum.$borotbl{$borough}.$pageidx.'.htm', '<style>form{margin-bottom:0px;margin-top:0px;}</style>'.$borofile);
-            $pageidx++;
         }
 
     }
     if(@boroughs > 1) { #partial 'a' tag HTML line, prefix added in later pass
         push(@lineshtml, 'href="'.$rtnum.'.htm">'.$routename.'</a>');
-        write_html('../docs/mn/'.($mob?'m/':'')."$rtnum.htm", $rtfile."\n");
+        write_html('../docs/mn/'.($mob?'m/':'')."$rtnum.htm", $rtfileheader.$rtfile."\n");
     } else { #jump directly to per-boro station page, suppress boro selection file
         push(@lineshtml, 'href="'.$rtnum.$borotbl{$boroughs[0]}.'.htm">'.$routename.'</a>');
     }
 }
+}
+
+sub stopid_to_tag { #$html = stopid_to_tag($name, $stopid, $dispname, $anchorname, $accesskey)
+my ($name, $stopid, $dispname, $anchorname, $accesskey) = @_;
+#name attr on input element is an anchor only on IE 6, not Openwave, Chrome, FF, or NN 3
+#id works on IE 6, Openwave, Chrome, FF, but not NN 3 (oh well)
+return ($mob?
+'<form action="http://m.mta.info/mt/as0.mta.info/mnr/mstations/station_status_display.cfm" method="post">
+<input value="'.$dispname.'" '.($anchorname?'id="'.$anchorname.'" ':'')
+.($accesskey?'accesskey='.$accesskey.' ':'').'type="submit">
+<input value="'.$stopid.','.$name.'" name="P_AVIS_ID" type="hidden">
+</form>'
+                                  :('<a '.($anchorname?'name="'.$anchorname.'" ':'')
+                                         .($accesskey?'accesskey='.$accesskey.' ':'')
+                .'href="http://as0.mta.info/mnr/mstations/station_status_display.cfm?P_AVIS_ID='
+                 .$stopid.','.$name.'">'.$dispname.'</a>'));
 }
 
 my $accesskeyidx = 1;
