495b485a43d80de77acabfc88d4fe76cb19b4615
bulk88 <bulk88@hotmail.com>
2021-08-26 17:15:40 -0400

    adjust tileMap.htm to use OUR tiles-saves xfer bandwidth

---
+++ sub/adj_tilepath.pl
@@ -0,0 +1,16 @@
+use strict;
+use File::Slurp;
+
+my $file = read_file( $ARGV[0], { binmode => ':raw' } );
+my $pos = index($file,'/*STARTINSERTPATH*/
+',0);
+die "bad starttag match" if $pos == -1;
+$pos += length('/*STARTINSERTPATH*/
+');
+my $endpos = index($file,'
+/*ENDINSERTPATH*/', $pos);
+die "bad endtag match" if $endpos == -1;
+substr($file,$pos, $endpos-$pos, '
+    tilePath = "neighborhoodMaps/" + tileDirectory + "/"
+');
+write_file($ARGV[0], {binmode => ':raw'}, $file);
