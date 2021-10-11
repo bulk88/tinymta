1a2ddfe8c5191f01d3d7c67cc8c958e1ab1e1cd3
bulk88 <bulk88@hotmail.com>
2021-10-11 15:37:08 -0400

    support old MSIE in plan.htm (subway)

--- sub/plan.js
+++ sub/plan.js
@@ -2,7 +2,7 @@ var originStation;
 onhashchange =
 // browser cache friendly hash argument
 function (evt) {
-alert("fired ohc");
+//alert("fired ohc");
   var stationHash = window.location.hash.substring(1);
   //not a "category" click but a final location click
   console.log("saw "+stationHash);
@@ -30,16 +30,38 @@ alert("fired ohc");
 };
 //onload event/back navigate
 (function () {
-debugger;
+//debugger;
   var stationHash = window.location.hash.substring(1);
   //if back from MTA domain, return to 1st station mode
   if(stationHash.indexOf('$') != -1) {
 		originStation = stationHash;
   }
-  alert("fired ol");
-  <!-- if(stationHash.indexOf('|') != -1) { -->
-		<!-- var ODArr = stationHash.split('|'); -->
-		<!-- originStation = ODArr[0]; -->
-		<!-- location.replace("#"+originStation) -->
-  <!-- } -->
+  //alert("fired ol");
+//  <!-- if(stationHash.indexOf('|') != -1) { -->
+//		<!-- var ODArr = stationHash.split('|'); -->
+//		<!-- originStation = ODArr[0]; -->
+//		<!-- location.replace("#"+originStation) -->
+//  <!-- } -->
+//this is a .js file in a head element, rest of doc not parsed yet
+  window.onload = function() {
+    if( window.navigator.userAgent.indexOf('MSIE ') > 0,1) {
+	var isDest2 = document.location.pathname.indexOf('plan2.htm') >= 0;
+	var i =0;
+	var arr = document.getElementsByTagName('a');
+	for(i=0; i< arr.length;i++) {
+		var e = arr[i];
+		//longer than "#SIS" "#FSK" must be a station loc
+		if(e.hash.length > 4) {
+			if(isDest2) {
+				//http://tripplanner.mta.info/MyTrip/js/landmarks.js ::: is field separator
+				e.pathname = e.pathname.replace('/plan.htm', '/planIE.htm');
+				e.hash = document.location.hash.substring(1)+":::"+e.hash.substring(1);
+			} else {
+				e.pathname = e.pathname.replace('/plan.htm', '/plan2.htm');
+			}
+		}
+		//debugger;
+	}
+    }
+  }
   })();
