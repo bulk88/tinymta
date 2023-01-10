c7b64d10b44e498d085c9e7981fa9efc28993887
bulk88 <bulk88@hotmail.com>
2023-01-10 09:40:52 -0500

    let LIRR stop.htm draw MNR trains
    -MNR JS API might die soon

--- lirr/mkstalist.pl
+++ lirr/mkstalist.pl
@@ -10,20 +10,25 @@ my $response;
 my $result;
 my %stajs; #ABBR to Full
 my %stapl; #GTFS (0 and up numbers) to ABBR (3 letter code)
+#var r = await fetch("https://backend-unified.mylirr.org/infrastructure", {headers: {'accept-version':'1.5'}});r=await r.json(); var s={}; r.stations.forEach(function(sta) {s[sta.code]=sta.name}); s;
 
 my $http = HTTP::Tiny->new(
     agent => 'Mozilla/5.0 (Windows NT 5.1; rv:41.0) Gecko/20100101 Firefox/41.0'
 );
 my $coder = Cpanel::JSON::XS->new->ascii->canonical(1);
 
-$response = $http->get('https://traintime.lirr.org/api/StationsAll');
+$response = $http->get('https://backend-unified.mylirr.org/infrastructure' ,{headers => {"accept-version" => '1.5'}});
 die "http req failed" if ! $response->{success};
 $result = $coder->decode($response->{content});
-my $stations = $result->{Stations};
-foreach my $key (keys %$stations) {
-    my $sta = $stations->{$key};
-    $stajs{$sta->{ABBR}} = $sta->{NAME};
-    $stapl{$key} = $sta->{ABBR};
+
+
+my $stations = $result->{stations};
+foreach my $sta (@$stations) {
+    $stajs{$sta->{code}} = $sta->{name};
+    #MNR overlaps with LIRR for these codes, IDK what sfStationId is
+    if($sta->{railroad} eq "LIRR") {
+      $stapl{$sta->{sfStationId}} = $sta->{code};
+    }
 }
 write_file('stations.js', {binmode => ':raw'}, 'var s='.$coder->encode(\%stajs).';');
 write_file('stations.pl', {binmode => ':raw'}, Dumper(\%stapl));
