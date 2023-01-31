33d14591feac07631657216986b6352738bd516d
bulk88 <bulk88@hotmail.com>
2023-01-31 14:07:28 -0500

    clean up & bugfix rstop.htm slow refresh
    -it was using subway stop.htm code path in the .pl file

--- adj_stoppath.pl
+++ adj_stoppath.pl
@@ -1,3 +1,4 @@
+#usage adj_stoppath.pl foo.htm [0|1|2|path type]
 use strict;
 use File::Slurp;
 
@@ -11,5 +12,5 @@ $pos += length('/*STARTINSERTPATH*/
 my $endpos = index($file,'
 /*ENDINSERTPATH*/', $pos);
 die "bad endtag match" if $endpos == -1;
-substr($file,$pos, $endpos-$pos, index($ARGV[0],'_') == -1 ? ($ARGV[1] ? '"status_.htm"' : '"stop_.htm"') : ($ARGV[1] ? '"status.htm"' : '"stop.htm"'));
+substr($file,$pos, $endpos-$pos, index($ARGV[0],'_') == -1 ? ($ARGV[1] == 2 ? '"rstop_.htm#"' : $ARGV[1] ? '"status_.htm"' : '"stop_.htm"') : ($ARGV[1] == 2 ? '"rstop.htm#"' : $ARGV[1] ? '"status.htm"' : '"stop.htm"'));
 write_file($ARGV[0], {binmode => ':raw'}, $file);
