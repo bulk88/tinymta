966d079d8042af93d90dc5bc4e9860582e1b9ce2
bulk88 <bulk88@hotmail.com>
2024-05-21 12:55:28 -0400

    index.htm try to have as.js execute earlier after I/O

--- spa.js
+++ spa.js
@@ -175,6 +175,19 @@ function STATE_PATHTYPE() {
       ac.swapCache();
       ac.onupdateready = null;
     };
+  function logEvent(event) {
+      console.log(event.type);
+  }
+  window.applicationCache.addEventListener('checking',logEvent,false);
+  window.applicationCache.addEventListener('noupdate',logEvent,false);
+  window.applicationCache.addEventListener('downloading',logEvent,false);
+  window.applicationCache.addEventListener('cached',logEvent,false);
+  window.applicationCache.addEventListener('updateready',logEvent,false);
+  window.applicationCache.addEventListener('obsolete',logEvent,false);
+  window.applicationCache.addEventListener('error',logEvent,false);
+  window.applicationCache.addEventListener('load',logEvent,false);
+  window.applicationCache.addEventListener('readystatechange',logEvent,false);
+window.applicationCache.addEventListener('success',logEvent,false);
     searchForAppCache = !hasLinkPF;
   }
 
